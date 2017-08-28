eb8d653a8dd0600dacff1ca54cc482a8ca8dfd33
Aaron Lee <aarontlee10@berkeley.edu>
2017-08-28 11:38:44 -0700

    pushed the state of the squares up to the parent (board)

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ class Square extends React.Component {
 	}
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.state.value}
       </button>
     );
@@ -19,12 +19,23 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} />;
+		return <Square value={this.state.squares[i]}  onClick={() => this.handleClick(i)}/>;  
+	}
+  constructor() {
+  		super();
+  		this.state = {
+  			squares: Array(9).fill(null)
+  		};
+  }
+
+  handleClick(i) {
+  	const squares = this.state.squares.slice();
+  	squares[i] = 'X';
+  	this.setState({squares: squares});
   }
 
   render() {
     const status = 'Next player: X';
-
     return (
       <div>
         <div className="status">{status}</div>
